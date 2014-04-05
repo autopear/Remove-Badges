688fd489092610c9992da7703a903740391c7e92
Merlin <Merlin@Merlins-MacBook-Pro.local>
2014-04-05 13:20:43 +0800

    tab to spaces

--- Tweak.mm
+++ Tweak.mm
@@ -44,48 +44,48 @@ static UIImage *listenerIcon = nil;
 static void PreferencesChangedCallback(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo) {
     if (preferences)
         [preferences release];
-	preferences = [[NSDictionary alloc] initWithContentsOfFile:PreferencesFilePath];
+    preferences = [[NSDictionary alloc] initWithContentsOfFile:PreferencesFilePath];
 }
 
 @implementation RemoveBadgesListener
 
 - (id)init {
-	if ((self = [super init])) {
-	}
-	return self;
+    if ((self = [super init])) {
+    }
+    return self;
 }
 
 - (void)activator:(LAActivator *)activator receiveEvent:(LAEvent *)event {
-	if ([%c(SBIconModel) respondsToSelector:@selector(sharedInstance)]) {
-		//iOS 4 & 5
-		SBIconModel *iconModel = (SBIconModel*)[%c(SBIconModel) sharedInstance];
-		for (NSString *identifier in [iconModel visibleIconIdentifiers]) {
-			SBIcon *icon = (SBIcon *)[iconModel applicationIconForDisplayIdentifier:identifier];
-			if (icon && [icon badgeNumberOrString]) {
-				[icon setBadge:nil];
-				NSLog(@"Badge removed: %@ (%@)", [icon displayName], identifier);
-			}
-		}
-	} else if ([%c(SBIconViewMap) respondsToSelector:@selector(homescreenMap)]) {
-		//iOS 6
-		for (NSString *identifier in [[[%c(SBIconViewMap) homescreenMap] iconModel] visibleIconIdentifiers]) {
-			SBIcon *icon = (SBIcon *)[[[%c(SBIconViewMap) homescreenMap] iconModel] applicationIconForDisplayIdentifier:identifier];
-			if (icon && [icon badgeNumberOrString]) {
-				[icon setBadge:nil];
-				NSLog(@"Badge removed: %@ (%@)", [icon displayName], identifier);
-			}
-		}
-	} else {
-		//Not implemented
-	}
+    if ([%c(SBIconModel) respondsToSelector:@selector(sharedInstance)]) {
+        //iOS 4 & 5
+        SBIconModel *iconModel = (SBIconModel*)[%c(SBIconModel) sharedInstance];
+        for (NSString *identifier in [iconModel visibleIconIdentifiers]) {
+            SBIcon *icon = (SBIcon *)[iconModel applicationIconForDisplayIdentifier:identifier];
+            if (icon && [icon badgeNumberOrString]) {
+                [icon setBadge:nil];
+                NSLog(@"Badge removed: %@ (%@)", [icon displayName], identifier);
+            }
+        }
+    } else if ([%c(SBIconViewMap) respondsToSelector:@selector(homescreenMap)]) {
+        //iOS 6
+        for (NSString *identifier in [[[%c(SBIconViewMap) homescreenMap] iconModel] visibleIconIdentifiers]) {
+            SBIcon *icon = (SBIcon *)[[[%c(SBIconViewMap) homescreenMap] iconModel] applicationIconForDisplayIdentifier:identifier];
+            if (icon && [icon badgeNumberOrString]) {
+                [icon setBadge:nil];
+                NSLog(@"Badge removed: %@ (%@)", [icon displayName], identifier);
+            }
+        }
+    } else {
+        //Not implemented
+    }
 }
 
 - (NSString *)activator:(LAActivator *)activator requiresLocalizedTitleForListenerName:(NSString *)listenerName {
-	return listenerTitle;
+    return listenerTitle;
 }
 
 - (NSString *)activator:(LAActivator *)activator requiresLocalizedDescriptionForListenerName:(NSString *)listenerName {
-	return listenerDescription;
+    return listenerDescription;
 }
 
 - (UIImage *)activator:(LAActivator *)activator requiresIconForListenerName:(NSString *)listenerName scale:(CGFloat)scale {
@@ -101,50 +101,50 @@ static void PreferencesChangedCallback(CFNotificationCenterRef center, void *obs
 @implementation RemoveBadgesInclusiveListener
 
 - (id)init {
-	if ((self = [super init])) {
+    if ((self = [super init])) {
         if (!preferences)
             preferences = [[NSDictionary alloc] initWithContentsOfFile:PreferencesFilePath];
         
         CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, PreferencesChangedCallback, CFSTR(PreferencesChangedNotification), NULL, CFNotificationSuspensionBehaviorCoalesce);
-	}
-	return self;
+    }
+    return self;
 }
 
 - (void)activator:(LAActivator *)activator receiveEvent:(LAEvent *)event {
-	if ([%c(SBIconModel) respondsToSelector:@selector(sharedInstance)]) {
-		//iOS 4 & 5
-		SBIconModel *iconModel = (SBIconModel*)[%c(SBIconModel) sharedInstance];
-		for (NSString *identifier in [iconModel visibleIconIdentifiers]) {
-			if ([[preferences objectForKey:[@"RemoveBadge-" stringByAppendingString:identifier]] boolValue]) {
+    if ([%c(SBIconModel) respondsToSelector:@selector(sharedInstance)]) {
+        //iOS 4 & 5
+        SBIconModel *iconModel = (SBIconModel*)[%c(SBIconModel) sharedInstance];
+        for (NSString *identifier in [iconModel visibleIconIdentifiers]) {
+            if ([[preferences objectForKey:[@"RemoveBadge-" stringByAppendingString:identifier]] boolValue]) {
                 SBIcon *icon = (SBIcon *)[iconModel applicationIconForDisplayIdentifier:identifier];
                 if (icon && [icon badgeNumberOrString]) {
                     [icon setBadge:nil];
                     NSLog(@"Badge removed: %@ (%@)", [icon displayName], identifier);
                 }
             }
-		}
-	} else if ([%c(SBIconViewMap) respondsToSelector:@selector(homescreenMap)]) {
-		//iOS 6
-		for (NSString *identifier in [[[%c(SBIconViewMap) homescreenMap] iconModel] visibleIconIdentifiers]) {
-			if ([[preferences objectForKey:[@"RemoveBadge-" stringByAppendingString:identifier]] boolValue]) {
+        }
+    } else if ([%c(SBIconViewMap) respondsToSelector:@selector(homescreenMap)]) {
+        //iOS 6
+        for (NSString *identifier in [[[%c(SBIconViewMap) homescreenMap] iconModel] visibleIconIdentifiers]) {
+            if ([[preferences objectForKey:[@"RemoveBadge-" stringByAppendingString:identifier]] boolValue]) {
                 SBIcon *icon = (SBIcon *)[[[%c(SBIconViewMap) homescreenMap] iconModel] applicationIconForDisplayIdentifier:identifier];
                 if (icon && [icon badgeNumberOrString]) {
                     [icon setBadge:nil];
                     NSLog(@"Badge removed: %@ (%@)", [icon displayName], identifier);
                 }
             }
-		}
-	} else {
-		//Not implemented
-	}
+        }
+    } else {
+        //Not implemented
+    }
 }
 
 - (NSString *)activator:(LAActivator *)activator requiresLocalizedTitleForListenerName:(NSString *)listenerName {
-	return listenerTitleInclusive;
+    return listenerTitleInclusive;
 }
 
 - (NSString *)activator:(LAActivator *)activator requiresLocalizedDescriptionForListenerName:(NSString *)listenerName {
-	return listenerDescriptionInclusive;
+    return listenerDescriptionInclusive;
 }
 
 - (UIImage *)activator:(LAActivator *)activator requiresIconForListenerName:(NSString *)listenerName scale:(CGFloat)scale {
@@ -160,52 +160,52 @@ static void PreferencesChangedCallback(CFNotificationCenterRef center, void *obs
 @implementation RemoveBadgesExclusiveListener
 
 - (id)init {
-	if ((self = [super init])) {
+    if ((self = [super init])) {
         if (!preferences)
             preferences = [[NSDictionary alloc] initWithContentsOfFile:PreferencesFilePath];
         
         CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, PreferencesChangedCallback, CFSTR(PreferencesChangedNotification), NULL, CFNotificationSuspensionBehaviorCoalesce);
-	}
-	return self;
+    }
+    return self;
 }
 
 - (void)activator:(LAActivator *)activator receiveEvent:(LAEvent *)event {
-	if ([%c(SBIconModel) respondsToSelector:@selector(sharedInstance)]) {
-		//iOS 4 & 5
-		SBIconModel *iconModel = (SBIconModel*)[%c(SBIconModel) sharedInstance];
-		for (NSString *identifier in [iconModel visibleIconIdentifiers]) {
-			if ([[preferences objectForKey:[@"KeepBadge-" stringByAppendingString:identifier]] boolValue])
-				continue;
+    if ([%c(SBIconModel) respondsToSelector:@selector(sharedInstance)]) {
+        //iOS 4 & 5
+        SBIconModel *iconModel = (SBIconModel*)[%c(SBIconModel) sharedInstance];
+        for (NSString *identifier in [iconModel visibleIconIdentifiers]) {
+            if ([[preferences objectForKey:[@"KeepBadge-" stringByAppendingString:identifier]] boolValue])
+                continue;
             
-			SBIcon *icon = (SBIcon *)[iconModel applicationIconForDisplayIdentifier:identifier];
-			if (icon && [icon badgeNumberOrString]) {
-				[icon setBadge:nil];
-				NSLog(@"Badge removed: %@ (%@)", [icon displayName], identifier);
-			}
-		}
-	} else if ([%c(SBIconViewMap) respondsToSelector:@selector(homescreenMap)]) {
-		//iOS 6
-		for (NSString *identifier in [[[%c(SBIconViewMap) homescreenMap] iconModel] visibleIconIdentifiers]) {
-			if ([[preferences objectForKey:[@"KeepBadge-" stringByAppendingString:identifier]] boolValue])
-				continue;
+            SBIcon *icon = (SBIcon *)[iconModel applicationIconForDisplayIdentifier:identifier];
+            if (icon && [icon badgeNumberOrString]) {
+                [icon setBadge:nil];
+                NSLog(@"Badge removed: %@ (%@)", [icon displayName], identifier);
+            }
+        }
+    } else if ([%c(SBIconViewMap) respondsToSelector:@selector(homescreenMap)]) {
+        //iOS 6
+        for (NSString *identifier in [[[%c(SBIconViewMap) homescreenMap] iconModel] visibleIconIdentifiers]) {
+            if ([[preferences objectForKey:[@"KeepBadge-" stringByAppendingString:identifier]] boolValue])
+                continue;
             
-			SBIcon *icon = (SBIcon *)[[[%c(SBIconViewMap) homescreenMap] iconModel] applicationIconForDisplayIdentifier:identifier];
-			if (icon && [icon badgeNumberOrString]) {
-				[icon setBadge:nil];
-				NSLog(@"Badge removed: %@ (%@)", [icon displayName], identifier);
-			}
-		}
-	} else {
-		//Not implemented
-	}
+            SBIcon *icon = (SBIcon *)[[[%c(SBIconViewMap) homescreenMap] iconModel] applicationIconForDisplayIdentifier:identifier];
+            if (icon && [icon badgeNumberOrString]) {
+                [icon setBadge:nil];
+                NSLog(@"Badge removed: %@ (%@)", [icon displayName], identifier);
+            }
+        }
+    } else {
+        //Not implemented
+    }
 }
 
 - (NSString *)activator:(LAActivator *)activator requiresLocalizedTitleForListenerName:(NSString *)listenerName {
-	return listenerTitleExclusive;
+    return listenerTitleExclusive;
 }
 
 - (NSString *)activator:(LAActivator *)activator requiresLocalizedDescriptionForListenerName:(NSString *)listenerName {
-	return listenerDescriptionExclusive;
+    return listenerDescriptionExclusive;
 }
 
 - (UIImage *)activator:(LAActivator *)activator requiresIconForListenerName:(NSString *)listenerName scale:(CGFloat)scale {
@@ -219,19 +219,19 @@ static void PreferencesChangedCallback(CFNotificationCenterRef center, void *obs
 @end
 
 __attribute__((constructor)) static void BAL_Main() {
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
-	NSBundle *bundle = nil;
-	bundle = [[NSBundle alloc] initWithPath:@"/Library/PreferenceLoader/Preferences/RemoveBadges/"];
+    NSBundle *bundle = nil;
+    bundle = [[NSBundle alloc] initWithPath:@"/Library/PreferenceLoader/Preferences/RemoveBadges/"];
 
-	listenerTitle = [NSLocalizedStringFromTableInBundle(@"Remove Badges", @"RemoveBadges", bundle, @"Remove Badges") retain];
-	listenerDescription = [NSLocalizedStringFromTableInBundle(@"Remove all app badges at once", @"RemoveBadges", bundle, @"Remove all app badges at once") retain];
+    listenerTitle = [NSLocalizedStringFromTableInBundle(@"Remove Badges", @"RemoveBadges", bundle, @"Remove Badges") retain];
+    listenerDescription = [NSLocalizedStringFromTableInBundle(@"Remove all app badges at once", @"RemoveBadges", bundle, @"Remove all app badges at once") retain];
 
-	listenerTitleInclusive = [NSLocalizedStringFromTableInBundle(@"Remove Badges (Inclusive)", @"RemoveBadges", bundle, @"Remove Badges (Inclusive)") retain];
-	listenerDescriptionInclusive = [NSLocalizedStringFromTableInBundle(@"Remove all app badges included in the list at once", @"RemoveBadges", bundle, @"Remove all app badges included in the list at once") retain];
+    listenerTitleInclusive = [NSLocalizedStringFromTableInBundle(@"Remove Badges (Inclusive)", @"RemoveBadges", bundle, @"Remove Badges (Inclusive)") retain];
+    listenerDescriptionInclusive = [NSLocalizedStringFromTableInBundle(@"Remove all app badges included in the list at once", @"RemoveBadges", bundle, @"Remove all app badges included in the list at once") retain];
 
-	listenerTitleExclusive = [NSLocalizedStringFromTableInBundle(@"Remove Badges (Exclusive)", @"RemoveBadges", bundle, @"Remove Badges (Exclusive)") retain];
-	listenerDescriptionExclusive = [NSLocalizedStringFromTableInBundle(@"Remove all app badges excluded in the list at once", @"RemoveBadges", bundle, @"Remove all app badges excluded in the list at once") retain];
+    listenerTitleExclusive = [NSLocalizedStringFromTableInBundle(@"Remove Badges (Exclusive)", @"RemoveBadges", bundle, @"Remove Badges (Exclusive)") retain];
+    listenerDescriptionExclusive = [NSLocalizedStringFromTableInBundle(@"Remove all app badges excluded in the list at once", @"RemoveBadges", bundle, @"Remove all app badges excluded in the list at once") retain];
 
     [bundle release];
 
@@ -240,14 +240,14 @@ __attribute__((constructor)) static void BAL_Main() {
     else
         listenerIcon = [[UIImage alloc] initWithContentsOfFile:@"/Library/PreferenceLoader/Preferences/RemoveBadges/icon.png"];
     
-	RemoveBadgesListener *listener = [[RemoveBadgesListener alloc] init];
-	[[LAActivator sharedInstance] registerListener:listener forName:@"com.autopear.removebadges"];
+    RemoveBadgesListener *listener = [[RemoveBadgesListener alloc] init];
+    [[LAActivator sharedInstance] registerListener:listener forName:@"com.autopear.removebadges"];
 
-	RemoveBadgesInclusiveListener *listenerInclusive = [[RemoveBadgesInclusiveListener alloc] init];
-	[[LAActivator sharedInstance] registerListener:listenerInclusive forName:@"com.autopear.removebadges.inclusive"];
+    RemoveBadgesInclusiveListener *listenerInclusive = [[RemoveBadgesInclusiveListener alloc] init];
+    [[LAActivator sharedInstance] registerListener:listenerInclusive forName:@"com.autopear.removebadges.inclusive"];
 
-	RemoveBadgesExclusiveListener *listenerExclusive = [[RemoveBadgesExclusiveListener alloc] init];
-	[[LAActivator sharedInstance] registerListener:listenerExclusive forName:@"com.autopear.removebadges.exclusive"];
+    RemoveBadgesExclusiveListener *listenerExclusive = [[RemoveBadgesExclusiveListener alloc] init];
+    [[LAActivator sharedInstance] registerListener:listenerExclusive forName:@"com.autopear.removebadges.exclusive"];
 
-	[pool release];
+    [pool release];
 }
